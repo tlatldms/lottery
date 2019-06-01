85b5bd54dc25f08d45fde93ec8f6a6975640c07e
안성환 <ash1209@ash1209.local>
2019-06-01 17:48:41 +0900

    5->6

--- Lottery.sol
+++ Lottery.sol
@@ -8,13 +8,13 @@ contract Lottery {
     uint private _tail;                             // tail index for queue
     mapping(uint => BettorInfo) private bets;       // self made queue for saving bettors' information
     uint constant BET_AMOUNT = 0.005 ether;         // bet amount will be fixed, may be changed
-    uint constant BLOCK_INTERVAL = 256;
+    //uint constant BLOCK_INTERVAL = 256;
     
     enum LotteryState {opened, closed}
     LotteryState state;
 
-    event BET(uint index, address payable bettor_address, uint amount, uint[5] numbers);
-    event CHECKBET(uint index, address payable bettor_address, uint amount, uint[5] numbers);
+    event BET(uint index, address payable bettor_address, uint amount, uint[6] numbers);
+    event CHECKBET(uint index, address payable bettor_address, uint amount, uint[6] numbers);
 
 
     constructor() public {
@@ -40,7 +40,7 @@ contract Lottery {
     struct BettorInfo {
         address payable bettor_address;
         uint amount;
-        uint[5] numbers;
+        uint[6] numbers;
     }
 
 
@@ -61,14 +61,14 @@ contract Lottery {
         return reward;
     }
 
-    function Bet(uint[5] memory betting_numbers) public payable returns (bool) {
+    function Bet(uint[6] memory betting_numbers) public payable returns (bool) {
         // check correct bet amount
         require(msg.value == BET_AMOUNT, "Not Enough ETH");
         
         // push the bet to the queue
         bets[_tail].bettor_address = msg.sender;
         bets[_tail].amount = msg.value;
-        for(uint i=0; i < 5; i++) {
+        for(uint i=0; i < 6; i++) {
             bets[_tail].numbers[i] = betting_numbers[i];
         }
         _tail++;
@@ -81,7 +81,7 @@ contract Lottery {
     // function for distributing the reward
     // if true, there is winner, if false, there is no winner
     function Distribute() onlyOwner IsLotteryClose public returns (bool){
-        uint[5] memory answer = random();
+        uint[6] memory answer = random();
         // address payable winner_address;
         address payable winner_address;
         uint flag;
@@ -90,11 +90,11 @@ contract Lottery {
         // win
         for(uint i=_head; i<_tail; i++) {
             flag = 0;
-            for(uint j=0; j<5; j++){
+            for(uint j=0; j<6; j++){
                 if(bets[i].numbers[j] != answer[j]) break;
                 else flag++;
             }
-            if(flag == 5) {
+            if(flag == 6) {
                 winner_address = bets[i].bettor_address;
             }
         }
@@ -133,7 +133,7 @@ contract Lottery {
         }
     }
     
-    function random() private view returns (uint[5] memory) {
+    function random() private view returns (uint[6] memory) {
         //return uint8(uint256(keccak256(block.timestamp)) % 50);
     }
     
