90253d96e84275c333b4553d0a8012e8b11349a1
sieun <tlatldms@naver.com>
2019-06-10 03:50:20 +0900

    Updates

---
+++ precache-manifest.44868c4222129864fc9dd7c94021b6dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c75a6c9aee439056e5fd1db0e28702",
+    "url": "/lottery/index.html"
+  },
+  {
+    "revision": "2fa2b8cba52f2662a599",
+    "url": "/lottery/static/css/main.4cfee7b2.chunk.css"
+  },
+  {
+    "revision": "39441db6ecc69ca6aec1",
+    "url": "/lottery/static/js/2.c22c824d.chunk.js"
+  },
+  {
+    "revision": "2fa2b8cba52f2662a599",
+    "url": "/lottery/static/js/main.2906a9d3.chunk.js"
+  },
+  {
+    "revision": "e6e0c3a0d9178fa18b34",
+    "url": "/lottery/static/js/runtime~main.50ff0365.js"
+  },
+  {
+    "revision": "450820bc0f8a373b20fc4d6bdc090d2b",
+    "url": "/lottery/static/media/1.450820bc.png"
+  }
+]);
