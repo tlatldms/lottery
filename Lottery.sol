a4440c1734bc1442fdf1d893a2f0ed2e5233e54c
zzunny97skkusosc <zzunny97@gmail.com>
2019-06-11 07:24:54 +0900

    delete notOwner modifier in register()

--- Lottery.sol
+++ Lottery.sol
@@ -204,7 +204,7 @@ contract Lottery {
 		return true;
 	}
 
-	function Register(string memory name, string memory id,string memory password) notOwner public {
+	function Register(string memory name, string memory id,string memory password) public {
 		require(Client[id].userAddress == address(0) );
 		Client[id].username=name;
 		Client[id].password=password;
